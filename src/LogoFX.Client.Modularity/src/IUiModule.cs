7d90fedc3a5019b61aafb0699e307d745853e36a
Gennady Verdel <godrose@gmail.com>
2017-07-17 16:22:55 +0300

    Moved portable project to the dedicated folder

---
+++ src/LogoFX.Client.Modularity/src/IUiModule.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace LogoFX.Client.Modularity
+{
+    /// <summary>
+    /// Represents user interface module.
+    /// </summary>
+    public interface IUiModule
+    {
+        /// <summary>
+        /// Module ID.
+        /// </summary>
+        string Id { get; }
+
+        /// <summary>
+        /// Module display name.
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// Module display order.
+        /// </summary>
+        int Order { get; }                
+    }
+
+    /// <summary>
+    /// Respresents user interface module with root view model.
+    /// </summary>
+    /// <typeparam name="TRootViewModel">Type of Root ViewModel</typeparam>
+    public interface IUiModule<out TRootViewModel> : IUiModule 
+        where TRootViewModel : IRootViewModel
+    {
+        /// <summary>
+        /// Root ViewModel
+        /// </summary>
+        TRootViewModel RootViewModel { get; }
+
+        /// <summary>
+        /// Explicit type of Root ViewModel
+        /// </summary>
+        Type RootModelType { get; }
+    }   
+}
