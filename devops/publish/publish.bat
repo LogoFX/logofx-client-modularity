4c0aa829992e98d3b133195a6d846d3b5d19fb59
Gennady Verdel <godrose@gmail.com>
2021-04-02 13:10:42 +0300

    devops: added means for package creation and storage #8

---
+++ devops/publish/publish.bat
@@ -0,0 +1,11 @@
+SET package_name=LogoFX.Client.Modularity
+SET package_version=2.2.0-rc2
+cd ../build
+call build.bat
+cd ../pack
+call ./pack.bat
+cd ../publish
+call ./copy.bat %package_name% %package_version% %1
+cd ../install
+call uninstall-global-single.bat %package_name% %package_version%
+cd ..
